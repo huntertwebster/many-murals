d603a6cd8f024eaf2ca36452f778b31bb2de2ecc
Hunter Webster <huntertwebster@gmail.com>
2021-09-28 13:37:35 -0500

    auth works, single artist can see ONLY their art on login

---
+++ server/routes/clearance.router.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+
+// GET grants access to the user depending on their access
+router.get('/', (req, res) => {
+    const access = `SELECT art_item.id, art_item.user_name, art_item.title, art_item.latitude,
+art_item.longitude, art_item.description, art_item.date, jsonb_agg(images) as images FROM public.user
+JOIN art_item ON public.user.id = art_item.user_id
+JOIN images on art_item.id = images.art_item_id
+WHERE "art_item"."user_id" = $1
+GROUP BY art_item.id, art_item.user_id, art_item.title, art_item.latitude, art_item.longitude, art_item.description, art_item.date;`
+    console.log('req.user:', req.user.id);
+    if (req.isAuthenticated()) {
+        pool.query(access, [req.user.id])
+            .then((results) => res.send(results.rows))
+            .catch((error) => {
+                console.log('Error making SELECT for artist:', error);
+                res.sendStatus(500);
+            });
+    } else {
+        res.sendStatus(403);
+    }
+});
+
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ const userRouter = require('./routes/user.router');
 const galleryRouter = require('./routes/gallery.router');
 const artistRouter = require('./routes/artist.router');
 const adminRouter = require('./routes/admin.router');
+const clearanceRouter = require('./routes/clearance.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -29,6 +30,7 @@ app.use('/api/user', userRouter);
 app.use('/api/gallery', galleryRouter);
 app.use('/api/artist', artistRouter);
 app.use('/api/admin', adminRouter);
+app.use('/api/clearance', clearanceRouter);
 
 // Serve static files
 app.use(express.static('build'));
