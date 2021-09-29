294419e58498f36f4dc9d1bdb55c3c25670ea93a
Hunter Webster <huntertwebster@gmail.com>
2021-09-29 12:55:50 -0500

    sagas put into one folder

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -1,14 +1,10 @@
 import { all } from 'redux-saga/effects';
-import gallerySaga from './gallery.saga';
 import loginSaga from './login.saga';
 import registrationSaga from './registration.saga';
 import userSaga from './user.saga';
 import gallerySaga from './gallery.saga';
-import deletePost from './artist.saga';
-import addGalleryPost from './artist.saga';
-import getArtistSaga from './artist.saga';
-import updatePost from './artist.saga';
-import deletePicture from './artist.saga';
+import artistSaga from './artist.saga';
+
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
 // This is imported in index.js as rootSaga
@@ -18,11 +14,7 @@ import deletePicture from './artist.saga';
 // and login triggers setting the user
 export default function* rootSaga() {
   yield all([
-    deletePicture(),
-    updatePost(),
-    deletePost(),
-    addGalleryPost(),
-    getArtistSaga(),
+    artistSaga(),
     gallerySaga(),
     loginSaga(), // login saga is now registered
     registrationSaga(),
--- src/redux/sagas/artist.saga.js
+++ src/redux/sagas/artist.saga.js
@@ -1,4 +1,4 @@
-import { put } from 'redux-saga/effects';
+import { put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 
 //fetches all artists from the DB
@@ -15,21 +15,8 @@ function* getArtistSaga() {
 
 };
 
-
-//posts an individual post to the DB
-function* addGalleryPost(action) {
-    console.log("----Inside the ADD_POST SAGA----", action);
-    try {
-        yield axios.post('/api/artist', action.payload);
-        yield put({ type: 'FETCH_GALLERY' });
-    } catch (err) {
-        console.log("addPost: Error FETCHING GALLERY for POST", err);
-    }
-};
-
-
-// deletes an entire post from the the gallery made from the artist profile
-function* deletePost(action) {
+//updating an entire post from the artists profile
+function* updatePost(action) {
     try {
 
         // passes all items from the server to the payload 
@@ -43,6 +30,16 @@ function* deletePost(action) {
     }
 }
 
+//posts an individual post to the DB
+function* addGalleryPost(action) {
+    console.log("----Inside the ADD_POST SAGA----", action);
+    try {
+        yield axios.post('/api/artist', action.payload);
+        yield put({ type: 'FETCH_GALLERY' });
+    } catch (err) {
+        console.log("addPost: Error FETCHING GALLERY for POST", err);
+    }
+};
 
 // deletes an individual picture from the artists post
 function* deletePicture(action) {
@@ -60,8 +57,8 @@ function* deletePicture(action) {
 }
 
 
-//updating an entire post from the artists profile
-function* updatePost(action) {
+// deletes an entire post from the the gallery made from the artist profile
+function* deletePost(action) {
     try {
 
         // passes all items from the server to the payload 
@@ -75,8 +72,15 @@ function* updatePost(action) {
     }
 }
 
-export default deletePicture;
-export default updatePost;
-export default deletePost;
-export default addGalleryPost;
-export default getArtistSaga;
+// watcher saga for my artist saga
+function* artistSaga() {
+    yield takeLatest('FETCH_ARTIST', getArtistSaga);
+    yield takeLatest('FETCH_GALLERY', updatePost);
+    yield takeLatest('FETCH_GALLERY', addGalleryPost);
+    yield takeLatest('FETCH_GALLERY', deletePicture);
+    yield takeLatest('FETCH_GALLERY', deletePost);
+    yield takeLatest('FETCH_GALLERY', updatePost);
+}
+
+
+export default artistSaga;
