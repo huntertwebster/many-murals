02c057a5f0703d8741e449925550c039a2f8b127
Hunter Webster <huntertwebster@gmail.com>
2021-11-27 11:34:57 -0600

    changes for cursors, hovers, and avatar

--- src/components/LogOutButton/LogOutButton.jsx
+++ src/components/LogOutButton/LogOutButton.jsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
+import React from "react";
+import { useDispatch } from "react-redux";
 
 function LogOutButton(props) {
   const dispatch = useDispatch();
   return (
     <>
-    <button
-      // This button shows up in multiple locations and is styled differently
-      // because it's styled differently depending on where it is used, the className
-      // is passed to it from it's parents through React props
-      className={props.className}
-      style={{
-        backgroundColor: "white",
-        border: "1px",
-        color: "black",
-        fontFamily: "Cormorant Garamond"
-      }}
-      onClick={() => dispatch({ type: 'LOGOUT' })}
-    >
-      Log Out
+      <button
+        // This button shows up in multiple locations and is styled differently
+        // because it's styled differently depending on where it is used, the className
+        // is passed to it from it's parents through React props
+        className={props.className}
+        style={{
+          backgroundColor: "transparent",
+          border: "1px",
+          color: "black",
+          fontFamily: "Cormorant Garamond",
+          paddingLeft: "10px",
+        }}
+        onClick={() => dispatch({ type: "LOGOUT" })}
+      >
+        Log Out
       </button>
-      </>
+    </>
   );
 }
 
